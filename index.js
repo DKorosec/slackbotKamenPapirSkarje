0ca6fc17afc2b4bcce243dc472b46921e28f9931
dkorosec <dominik.korosec@gmail.com>
2016-12-29 15:32:31 +0100

    added async response to slack

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 "use strict"
 var RPSGame = require('./rpsgame');
-var rpsGame = null;
 var app = require('express')();
 var bodyParser = require("body-parser");
+var request = require('request');
+
+var rpsGame = null;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 5000));
@@ -23,7 +25,15 @@ app.post('/kps',(req,res,next)=>{
   const pick = req.body.text.toLowerCase();
   const json = rpsGame.playWith(username,pick);
 
-  res.status(200).json(json);
+  res.status(200).end();
+
+  request.post(req.body.response_url,
+    { json },
+    (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+    }
+  });
 });
 
 app.listen(app.get('port'), ()=>{
